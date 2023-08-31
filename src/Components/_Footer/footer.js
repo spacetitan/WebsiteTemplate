0a410b713e9cd79fc8eb26ed6363a33447498269
Benjamin Navarro <bennavarro3@gmail.com>
2023-08-30 17:11:24 -0700

    Finished header and footer

--- src/Components/_Footer/footer.js
+++ src/Components/_Footer/footer.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import Styles from './footer.module.css';
+import { Link } from "react-router-dom";
 
 const footer = () => {
 return (
     <>
         <div className={Styles.footerContainer}>
-            
+            <Link to={'/'} className={Styles.footerBtnLink}>
+                <button className={Styles.footerButton}><img className={Styles.footerLogo} src="../Images/MUImage.png" alt=''/></button>
+            </Link>
+
+            <div className={Styles.footerTextContainer}>
+                <p className={Styles.footerText}>
+                    Feel free to email me at <Link to={'mailto:ben@aggressorlabs.com'} className={Styles.footerLink}>ben@aggressorlabs.com</Link>
+                </p>
+
+                <p className={Styles.footerCopyrightText}>
+                Copyright © Year Benjamin Navarro
+                </p>
+            </div>
+
+            <div className={Styles.footerSpacer}/>
         </div>
     </>
 );};
