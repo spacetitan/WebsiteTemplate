d3f8186ea07a235cd88c9fd9211a2224776c122c
Benjamin Navarro <bennavarro3@gmail.com>
2023-09-04 14:07:38 -0700

    added download link template

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -7,8 +7,14 @@ const Home = () => {
 return (
     <>
         <div className={Styles.homeContainer}>
-            <div className={Styles.homeOverlayCard}>
-                <OverlayCard title={"Title"}/>
+            <div className={Styles.homeOverlayCardContainer}>
+                <div className={Styles.homeOverlayCard}><OverlayCard title={"Card 1"}/></div>
+                <div className={Styles.homeOverlayCard}><OverlayCard title={"Card 2"}/></div>
+                <div className={Styles.homeOverlayCard}><OverlayCard title={"Card 3"}/></div>
+            </div>
+            <div className={Styles.homeDownloadLink}>
+                {/*https://websiteAddress.com/Images/cardTest.png*/}
+            <a href='http://localhost:3000/Images/cardTest.png' download={'cardTest.png'} target='_blank'>png download</a>
             </div>
         </div>
     </>
